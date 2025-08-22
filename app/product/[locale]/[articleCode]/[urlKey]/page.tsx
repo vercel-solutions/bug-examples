1d46d3d8694b4e0acfa239b818c9196566de774e
darekrossman <darek@subpopular.dev>
2025-08-22 13:39:48 -0400

    sync

--- app/product/[locale]/[articleCode]/[urlKey]/page.tsx
+++ app/product/[locale]/[articleCode]/[urlKey]/page.tsx
@@ -3,6 +3,26 @@ import Link from "next/link";
 
 export const revalidate = 600;
 
+export async function generateStaticParams() {
+  return [
+    {
+      locale: "fr",
+      articleCode: "123456",
+      urlKey: "product-z",
+    },
+    {
+      locale: "fr",
+      articleCode: "123456",
+      urlKey: "product-x",
+    },
+    {
+      locale: "fr",
+      articleCode: "123456",
+      urlKey: "product-y",
+    },
+  ];
+}
+
 export default async function ProductPage({
   params,
 }: {
