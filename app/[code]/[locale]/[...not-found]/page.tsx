e098b39b883c9020903bd3ff57d213749413caf6
darekrossman <darek@subpopular.dev>
2025-09-08 17:31:24 -0400

    sync

--- app/[code]/[locale]/[...not-found]/page.tsx
+++ app/[code]/[locale]/[...not-found]/page.tsx
@@ -2,9 +2,11 @@ import { notFound } from "next/navigation";
 
 export const dynamic = "error";
 
-export default async function CatchAllPage(
-  params: Promise<Record<string, unknown>>,
-) {
+export default async function CatchAllPage({
+  params,
+}: {
+  params: Promise<Record<string, unknown>>;
+}) {
   console.log("catch all page params", await params);
   notFound();
 }
