82d21c8c811e67bc8c9b23e29ce424d0e625e42e
darekrossman <darek@subpopular.dev>
2025-09-09 09:29:04 -0400

    explain

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function HomePage({
   console.log("home page params", _params);
   return (
     <div className="flex flex-col gap-4 p-8">
-      <h1>Locale Home Page: {_params.locale}</h1>
+      <h1>Home</h1>
 
       <ul>
         <li>
@@ -28,6 +28,14 @@ export default async function HomePage({
           </Link>
         </li>
       </ul>
+
+      <p>
+        This demo illustrates a strange quirk where naming a catch-all route{" "}
+        <code>[...not-found]</code> and calling <code>notFound()</code> in it's
+        page will throw you out to the root <code>not-found.tsx</code> instead
+        of the nearest one, causing params in the inner layout and catch-all
+        page to be lost on the way.
+      </p>
     </div>
   );
 }
